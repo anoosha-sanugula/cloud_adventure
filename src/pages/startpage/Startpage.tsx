8e8e852ee3c495ca9f0196bd0728a7855dbd52e1
goldie <goldie@Anoosha-Sanugula.local>
2025-03-12 14:29:52 +0530

    Implement starting page of the app

---
+++ src/pages/startpage/Startpage.tsx
@@ -0,0 +1,12 @@
+import "./Startpage.css";
+const Startpage = () => {
+  return (
+    <div>
+      <h1>Hey!!! Welcome to Cloud_Adventure</h1>
+      <button>Register</button>
+      <button>Login</button>
+    </div>
+  );
+};
+
+export default Startpage;
