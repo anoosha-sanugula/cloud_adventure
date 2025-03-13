3bc645dbc3b40b644b1a67dfdc80dce244de90fb
goldie <goldie@Anoosha-Sanugula.local>
2025-03-13 08:04:37 +0530

    Add navigation to the respective register and login pages

--- src/pages/startpage/Startpage.tsx
+++ src/pages/startpage/Startpage.tsx
@@ -1,10 +1,12 @@
 import "./Startpage.css";
+import {useNavigate } from "react-router-dom";
 const Startpage = () => {
+  const navigate = useNavigate();
   return (
     <div>
       <h1>Hey!!! Welcome to Cloud_Adventure</h1>
-      <button>Register</button>
-      <button>Login</button>
+      <button onClick={() => navigate("register")}>Register</button>
+      <button onClick={() => navigate("login")}>Login</button>
     </div>
   );
 };
