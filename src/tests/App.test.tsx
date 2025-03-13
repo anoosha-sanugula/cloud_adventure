1ee4dcdfec02e5c398ff88e2ff5af25b8aa2caef
goldie <goldie@Anoosha-Sanugula.local>
2025-03-13 08:00:43 +0530

    Include tests for register page rendering

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,7 +1,20 @@
 import { expect, test } from "vitest";
 import { render, screen } from "@testing-library/react";
 import App from "../App";
+import { BrowserRouter, MemoryRouter } from "react-router-dom";
 test("Testing the app component", () => {
-  render(<App />);
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
   expect(screen.getByText("Hey!!! Welcome to Cloud_Adventure")).toBeDefined();
+});
+test('renders Register page on "/register" route', async () => {
+  render(
+    <MemoryRouter initialEntries={["/register"]}>
+      <App />
+    </MemoryRouter>
+  );
+  expect(screen.getByText(/Register Here!/i)).toBeDefined();
 });
