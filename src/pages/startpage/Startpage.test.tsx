c0c99051a4b384c976b116814343fec62c5fb104
goldie <goldie@Anoosha-Sanugula.local>
2025-03-13 08:04:01 +0530

    Add tests for startpage navigation

--- src/pages/startpage/Startpage.test.tsx
+++ src/pages/startpage/Startpage.test.tsx
@@ -1,7 +1,13 @@
-import {expect, test} from "vitest";
+import { expect, test } from "vitest";
 import Startpage from "./Startpage";
-import { render,screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+
 test("testing the startpage text", () => {
-    render(<Startpage />);
-    expect(screen.getByText("Hey!!! Welcome to Cloud_Adventure")).toBeDefined();
+  render(
+    <BrowserRouter>
+      <Startpage />
+    </BrowserRouter>
+  );
+  expect(screen.getByText("Hey!!! Welcome to Cloud_Adventure")).toBeDefined();
 });
